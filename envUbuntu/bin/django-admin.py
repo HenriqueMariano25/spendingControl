0a75d96abc5a6a114549ded8e5d9d450667afc80
Henrique Mariano <henrique.marino25@hotmail.com>
2020-05-19 21:13:46 -0300

    Initial project files

---
+++ envUbuntu/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/henrique/Documentos/Desenvolvimento/Python/spendingControl/envUbuntu/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
